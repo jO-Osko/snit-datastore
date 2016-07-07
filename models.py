f0e24ed613bf09684f9e90aa9b4193bcb293160c
Filip Koprivec <koprivec.filip@gmail.com>
2016-07-07 16:43:12 +0200

    Dodal email

--- models.py
+++ models.py
@@ -6,3 +6,16 @@ from google.appengine.ext import ndb
 class Sporocilo(ndb.Model):
     besedilo = ndb.StringProperty()  # Za zacetek mu dajmo zgolj eno vrednost -> besedilo tega sporocila
     nastanek = ndb.DateTimeProperty(auto_now_add=True)  # Shranimo si nastanek (to naredimo kar avtomatsko)
+
+
+#mozne razsiritve
+class Email(ndb.Model):
+    zadeva = ndb.StringProperty()
+    posiljatelj = ndb.StringProperty()
+    sporocilo = ndb.TextProperty()  # za daljse besedilo
+    poslano = ndb.DateTimeProperty(auto_now_add=True)
+
+    pomembno = ndb.BooleanProperty()
+    prebrano = ndb.BooleanProperty()
+
+    stevilo_prejemnikov = ndb.IntegerProperty()
