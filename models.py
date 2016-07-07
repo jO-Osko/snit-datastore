05e5692cb788db3de2519550a41762a965ef5d52
Filip Koprivec <koprivec.filip@gmail.com>
2016-07-07 16:14:17 +0200

    Dodal datum in cas v sporocilo

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from google.appengine.ext import ndb
 
 
 # Ustvarimo osnovni model za Sporocilo
+# Sporocilo avtomatsko dobi tudi id() metodo, ki je zelo uporabna za dolocanje
 class Sporocilo(ndb.Model):
     besedilo = ndb.StringProperty()  # Za zacetek mu dajmo zgolj eno vrednost -> besedilo tega sporocila
-
+    nastanek = ndb.DateTimeProperty(auto_now_add=True)  # Shranimo si nastanek (to naredimo kar avtomatsko)
