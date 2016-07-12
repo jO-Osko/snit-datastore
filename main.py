de30a92c5e01a9127ae614b26812570b5fd37c41
Filip Koprivec <koprivec.filip@gmail.com>
2016-07-12 14:56:33 +0200

    Tecaj

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 import jinja2
 import webapp2
 
+from models import Sporocilo
+
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
 
@@ -36,10 +38,36 @@ class IndexHandler(BaseHandler):
 class RezultatHandler(BaseHandler):
     def post(self):
         sporocilo = self.request.get("vnos")
+
+        sporocilo_v_bazi = Sporocilo(besedilo=sporocilo)
+        sporocilo_v_bazi.put()
+
         return self.write("Vpisal si: " + sporocilo)
 
 
+class SeznamSporocilHandler(BaseHandler):
+    def get(self):
+
+        vsa_sporocila = Sporocilo.query().fetch()
+
+        params = {"sporocila": vsa_sporocila}
+
+        return self.render_template("vsa_sprocila.html", params=params)
+
+
+class PosameznoSporociloHandler(BaseHandler):
+    def get(self, sporocilo_id):
+        sporocilo = Sporocilo.get_by_id(int(sporocilo_id))
+
+        params = {"sporocilo": sporocilo}
+
+        return self.render_template("posamezno_sporocilo.html",
+                                    params=params)
+
+
 app = webapp2.WSGIApplication([
-    ('/', IndexHandler),
-    ('/rezultat', RezultatHandler),
+    webapp2.Route('/', IndexHandler),
+    webapp2.Route('/rezultat', RezultatHandler),
+    webapp2.Route('/vsa_sporocila', SeznamSporocilHandler),
+    webapp2.Route('/sporocilo/<sporocilo_id:\\d+>', PosameznoSporociloHandler)
 ], debug=True)
